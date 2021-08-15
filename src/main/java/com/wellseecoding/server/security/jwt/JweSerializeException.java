adb525ac6fb4c523d3cc0513c8bb07cdd6c7b8ff
youmustgo <iluvtrollhd@gmail.com>
2021-08-15 18:58:28 +0900

    Add a JWE mapper

---
+++ src/main/java/com/wellseecoding/server/security/jwt/JweSerializeException.java
@@ -0,0 +1,7 @@
+package com.wellseecoding.server.security.jwt;
+
+public class JweSerializeException extends RuntimeException {
+    public JweSerializeException(Throwable cause) {
+        super(cause);
+    }
+}
