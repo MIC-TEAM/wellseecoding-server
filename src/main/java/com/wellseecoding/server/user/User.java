8d4f0b57a32a05e00f046f5a843337e877f77c58
youmustgo <iluvtrollhd@gmail.com>
2021-08-15 20:27:53 +0900

    Add missing field for email

--- src/main/java/com/wellseecoding/server/user/User.java
+++ src/main/java/com/wellseecoding/server/user/User.java
@@ -13,6 +13,7 @@ public class User {
     private Long id;
     private String username;
     private String password;
+    private String email;
     @Column(name = "refresh_token")
     private String refreshToken;
 }
